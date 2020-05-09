1ba3726aa2e81371e2ea0be802a40417064eaead
lokeshpayani <lokeshpayani7@gmail.com>
2020-05-09 21:15:21 +0530

    06-Logical-AND: initial commit

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-let password = "Hell";
+let password = "CheckenGel";
 
-if(password.length >= 6){
-  if(password.indexOf(' ') === -1){
-    console.log('Valid Password!');
-  }else{
-    console.log('Password is long enough, but cannot contain space');
-  }
+if(password.length >= 6 && password.indexOf(' ') === -1){
+    console.log('Vaild Password');
 }else {
-  console.log('Password must be long!');
+    console.log('Invalid Password');
+}
+
+
+let num = "6";
+
+if( num >= 1 && num <= 10 ){
+    console.log('Number is between 1 and 10');
+}else {
+    console.log('Please guess the number between 1 to 10');
 }
