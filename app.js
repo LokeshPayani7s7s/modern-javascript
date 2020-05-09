2b36eac029fb4badb04eff6e67cf26e8419a9002
lokeshpayani <lokeshpayani7@gmail.com>
2020-05-09 19:59:34 +0530

    05-Truthy-Falsy: initial commit

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-alert('Hi this is my first script');
+let mestry = 0;
+
+// FALSY VALUES
+
+      // false
+      // 0
+      // ""(empty string)
+      // null
+      // undefined
+      // NaN
+
+if(mestry) {
+  console.log('TRUTHY');
+}else {
+  console.log('FALSY');
+}
+
+
+let loggedInUser = 'ABC123';
+
+if (loggedInUser) {
+  console.log('YOU ARE LOGGED IN');
+} else{
+  console.log("PLEASE LOG IN")
+}
