7e0feaa52935b3defa0d110fdd2035c25cabbd85
lokeshpayani <lokeshpayani7@gmail.com>
2020-05-09 19:43:50 +0530

    04-NestedLoops: initial commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert('Hi this is my first script');
+let password = "Hell";
+
+if(password.length >= 6){
+  if(password.indexOf(' ') === -1){
+    console.log('Valid Password!');
+  }else{
+    console.log('Password is long enough, but cannot contain space');
+  }
+}else {
+  console.log('Password must be long!');
+}
