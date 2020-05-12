f87453a7c932bab8fdb2940ac4c70b4761618f04
lokeshpayani <lokeshpayani7@gmail.com>
2020-05-12 19:41:57 +0530

    14.concat: initial commit

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-let password = "Hell";
+let fruits = ['apple', 'bannana'];
+let veggies = ['asparagus', 'brussel sprouts'];
+let meet = ['steak', 'chicken breast'];
 
-if(password.length >= 6){
-  if(password.indexOf(' ') === -1){
-    console.log('Valid Password!');
-  }else{
-    console.log('Password is long enough, but cannot contain space');
-  }
-}else {
-  console.log('Password must be long!');
-}
+
+console.log(fruits.concat(veggies));  // ["apple", "bannana", "asparagus", "brussel sprouts"]
+console.log(veggies.concat(fruits));  // ["asparagus", "brussel sprouts", "apple", "bannana"]
+console.log(veggies.concat(fruits, meet)); //  ["asparagus", "brussel sprouts", "apple", "bannana", "steak", "chicken breast"]
