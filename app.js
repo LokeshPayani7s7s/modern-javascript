855a79a3dfd13c98b1c6ded01498c16337fe3ce1
lokeshpayani <lokeshpayani7@gmail.com>
2020-05-16 12:54:16 +0530

    20.Nested-Arrays: initial commit

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-let password = "Hell";
+const animalPairs = [
+  ['doe', 'buck'],
+  ['ewe', 'ram'],
+  ['peahen', 'peacock'],
+  ['loki', ['lokesh','payani']]
+]
 
-if(password.length >= 6){
-  if(password.indexOf(' ') === -1){
-    console.log('Valid Password!');
-  }else{
-    console.log('Password is long enough, but cannot contain space');
-  }
-}else {
-  console.log('Password must be long!');
-}
+// to access the array values
+
+console.log(animalPairs[0]); // ["doe", "buck"]
+console.log(animalPairs[0][0]);// doe
+console.log(animalPairs[0][1]);// buck
+
+
+console.log(animalPairs[1]); //  ['ewe', 'ram']
+console.log(animalPairs[1][0]); // ewe
+console.log(animalPairs[1][1]); // ram
+
+
+console.log(animalPairs[2]); // ['peahen', 'peacock']
+console.log(animalPairs[2][0]); // peahen
+console.log(animalPairs[2][1]); // peacock
+
+console.log(animalPairs[3]); // ["loki", Array(2)]
+console.log(animalPairs[3][0]); // loki
+console.log(animalPairs[3][1]); // ["lokesh", "payani"]
+
+// to Update
+
+console.log(animalPairs[0]); // ["doe", "buck"]
+console.log(animalPairs[0][0] = 'stag');// stag
+console.log(animalPairs[0]); // ["stag", "buck"]
